654218dd136fc16819e9e8fc570a3319acc6f269
DaamiAle <daamiale.recoil@gmail.com>
2021-08-30 21:27:52 -0300

    Base project

---
+++ src/test/java/com/somosmas/alkemy/AlkemyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.somosmas.alkemy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlkemyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
