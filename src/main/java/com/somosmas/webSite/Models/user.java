0e0110133dc2813ffad677f8026fc7ba91acd1e6
Juan Ignacio Caballero <juan.ci.caballero@gmail.com>
2021-08-31 01:18:31 -0300

    User model created

--- src/main/java/com/somosmas/webSite/Models/user.java
+++ src/main/java/com/somosmas/webSite/Models/user.java
@@ -1,5 +1,19 @@
 package com.somosmas.webSite.Models;
 
-public class user {
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "user")
+public class User {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(unique = true)
+    private Long idUser;
     
 }
