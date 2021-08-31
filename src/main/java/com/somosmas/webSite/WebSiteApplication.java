4610a1be3ebc42a30039b454027afa281686b87a
DaamiAle <daamiale.recoil@gmail.com>
2021-08-30 21:48:18 -0300

    Fixes base project name

--- src/main/java/com/somosmas/webSite/WebSiteApplication.java
+++ src/main/java/com/somosmas/webSite/WebSiteApplication.java
@@ -1,13 +1,13 @@
-package com.somosmas.alkemy;
+package com.somosmas.webSite;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class AlkemyApplication {
+public class WebSiteApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(AlkemyApplication.class, args);
+		SpringApplication.run(WebSiteApplication.class, args);
 	}
 
 }
