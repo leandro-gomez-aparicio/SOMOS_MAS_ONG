89f270f3eab9789e40c951df9159a7beca41b8b6
DaamiAle <daamiale.recoil@gmail.com>
2021-08-31 01:02:55 -0300

    Fixed TestClass

--- src/test/java/com/somosmas/webSite/WebSiteApplicationTests.java
+++ src/test/java/com/somosmas/webSite/WebSiteApplicationTests.java
@@ -1,10 +1,10 @@
-package com.somosmas.alkemy;
+package com.somosmas.webSite;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class AlkemyApplicationTests {
+class WebSiteApplicationTests {
 
 	@Test
 	void contextLoads() {
