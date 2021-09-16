f0b844212fcc41fcbc1309ac9991d6899d9b7d12
Magali Kain <mkain@mulesoft.com>
2021-09-16 18:45:52 -0300

    Fixing RegisterUser integration test

--- src/test/java/com/somosmas/app/integration/RegisterUserIntegrationTest.java
+++ src/test/java/com/somosmas/app/integration/RegisterUserIntegrationTest.java
@@ -1,24 +1,43 @@
 package com.somosmas.app.integration;
 
 
+import com.somosmas.app.config.security.RoleType;
+import com.somosmas.app.model.entity.Role;
+import com.somosmas.app.model.entity.User;
 import com.somosmas.app.model.request.UserDetailsRequest;
 import com.somosmas.app.model.response.UserDetailsResponse;
+import com.somosmas.app.repository.IRoleRepository;
+import com.somosmas.app.repository.IUserRepository;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.when;
+
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class RegisterUserIntegrationTest extends BaseIntegrationTest {
 
+    @MockBean
+    IRoleRepository roleRepository;
+
+    @MockBean
+    IUserRepository userRepository;
+
     @Test
     public void shouldRegisterUser() {
+        when(roleRepository.findByName(eq(RoleType.ROLE_USER.name()))).thenReturn(stubRole());
+        when(userRepository.save(any(User.class))).thenReturn(stubUser());
+
         UserDetailsRequest request = new UserDetailsRequest();
         request.setEmail("paul@wayne.com");
         request.setPassword("abc1234&");
@@ -34,4 +53,21 @@ public class RegisterUserIntegrationTest extends BaseIntegrationTest {
         Assert.assertEquals("paul@wayne.com", response.getBody().getEmail());
     }
 
+    private Role stubRole() {
+        Role role = new Role();
+        role.setIdRole(1L);
+        role.setName(RoleType.ROLE_USER.name());
+        return role;
+    }
+
+    private User stubUser() {
+        User user = new User();
+        user.setIdUser(1L);
+        user.setEmail("paul@wayne.com");
+        user.setPassword("abc1234&");
+        user.setFirstName("Paul");
+        user.setLastName("Wayne");
+        user.setPhoto("");
+        return user;
+    }
 }
